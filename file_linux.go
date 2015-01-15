58892f78475d12efb879b62641b2532a1fff9d66
Marc MacLeod <Marc@Marcs-MacBook-Pro.local>
2015-01-14 20:19:54 -0800

    dont nest log directory for linux

--- file_linux.go
+++ file_linux.go
@@ -8,14 +8,7 @@ import (
 
 // TODO
 func NewFileLogger(config *LogConfig) (Logger, error) {
-	path := "/var/log/" + strings.ToLower(config.DirectoryName)
-	err := os.MkdirAll(path, os.FileMode(0777))
-	if err != nil {
-		stdLog.Fatalf("error creating log directory: %v", err)
-		return nil, err
-	}
-
-	f, err := os.OpenFile(path+"/log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("/var/log/"+strings.ToLower(config.DirectoryName)+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		stdLog.Fatalf("error opening file: %v", err)
 		return nil, err
