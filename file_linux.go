0a3cb73b0a080a16da2cbecdab0fbebb800e96c7
Marc MacLeod <Marc@Marcs-MacBook-Pro.local>
2015-01-14 20:41:43 -0800

    store logs in hidden home folder

--- file_linux.go
+++ file_linux.go
@@ -3,12 +3,24 @@ package log
 import (
 	stdLog "log"
 	"os"
+	"os/user"
 	"strings"
 )
 
-// TODO
 func NewFileLogger(config *LogConfig) (Logger, error) {
-	f, err := os.OpenFile("/var/log/"+strings.ToLower(config.DirectoryName)+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	currentUser, err := user.Current()
+	if err != nil {
+		stdLog.Fatalf("error reading current system user: %v", err)
+		return nil, err
+	}
+	path := "/home/" + currentUser.Username + "/." + strings.ToLower(config.DirectoryName)
+	err = os.MkdirAll(path, os.FileMode(0777))
+	if err != nil {
+		stdLog.Fatalf("error creating support directory: %v", err)
+		return nil, err
+	}
+
+	f, err := os.OpenFile(path+"/log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		stdLog.Fatalf("error opening file: %v", err)
 		return nil, err
