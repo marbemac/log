261bc5c4f903bfca209635c8cff3bb8998ae411d
Marc MacLeod <Marc@Marcs-MacBook-Pro.local>
2015-01-14 20:12:58 -0800

    add linux support

--- file_linux.go
+++ file_linux.go
@@ -2,9 +2,24 @@ package log
 
 import (
 	"os"
+	"strings"
 )
 
 // TODO
 func NewFileLogger(config *LogConfig) (Logger, error) {
-	return &fileLogger{w: os.Stdout}, nil
+	path := "/var/log/" + strings.ToLower(config.DirectoryName)
+	err = os.MkdirAll(path, os.FileMode(0777))
+	if err != nil {
+		stdLog.Fatalf("error creating log directory: %v", err)
+		return nil, err
+	}
+
+	f, err := os.OpenFile(path+"/log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		stdLog.Fatalf("error opening file: %v", err)
+		return nil, err
+	}
+	stdLog.SetOutput(f)
+
+	return &fileLogger{w: f}, nil
 }
